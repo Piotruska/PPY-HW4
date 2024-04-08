7d68d5f40981ba9ffac64279b467d63ee230594f
s27051 <s27051@pjwstk.edu.pl>
2024-04-08 18:31:31 +0200

    Added Class TicketReservatiomSystem

--- Task2.py
+++ Task2.py
@@ -24,4 +24,34 @@ class Ticket:
         self.seat_number = seat_number
 
 
+class TicketReservationSystem:
+    def __init__(self):
+        self.available_events = []
+        self.reserved_tickets = []
+
+    def add_event(self, event):
+        self.available_events.append(event)
+
+    def show_events(self):
+        print("Available Events:")
+        for index, event in enumerate(self.available_events):
+            print(f"{index + 1}. {event.name}")
+
+    def make_reservation(self, event_index, seat_number):
+        if 1 <= event_index <= len(self.available_events):
+            event = self.available_events[event_index - 1]
+            ticket = Ticket(event, seat_number)
+            self.reserved_tickets.append(ticket)
+            print(f"Reservation made for {event.name}, Seat Number: {seat_number}")
+        else:
+            print("Invalid event index.")
+
+    def cancel_reservation(self, ticket):
+        if ticket in self.reserved_tickets:
+            self.reserved_tickets.remove(ticket)
+            print("Reservation canceled.")
+        else:
+            print("Ticket not found in reservations.")
+
+
 
