f92a83c34bd9ed924b78736f83c2eb6a9da537b2
s27051 <s27051@pjwstk.edu.pl>
2024-04-08 18:30:34 +0200

    Added Class Concert

--- Task2.py
+++ Task2.py
@@ -8,4 +8,14 @@ class Event:
         return f"Event: {self.name}\nDate: {self.date}\nTicket Price: ${self.ticket_price}"
 
 
+class Concert(Event):
+    def __init__(self, name, date, ticket_price, max_participants):
+        super().__init__(name, date, ticket_price)
+        self.max_participants = max_participants
+
+    def get_info(self):
+        event_info = super().get_info()
+        return f"{event_info}\nMax Participants: {self.max_participants}"
+
+
 
